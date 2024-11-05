9f9dfe4a5ada9f88c88cc93c3afca7bed26e1597
WwWw201 <hchworkplace@gmail.com>
2024-11-05 18:21:39 +0800

    edited

--- client/src/nav/HOME/Product.js
+++ client/src/nav/HOME/Product.js
@@ -1,20 +1,63 @@
 import Carousel from 'react-material-ui-carousel'
 import React from 'react'
 import './Product.css'
+import { products } from './prodcut_detail';
 import { Divider } from '@mui/material';
+import { NavLink } from "react-router-dom";
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 1000, min: 900 },
+        items: 3,
+        slideToSlide:3
+    }
+};
 const Product = () => {
     return (
-        
-            <div>
-                <div className='head'>
-                    <h3 style={{ fontSize: "30px" }}>Product</h3>
-                    <button className="all_btn" style={{ fontSize: "30px" }}>View All</button>
-                </div>
-                <Divider sx={{ bgcolor: "black" }}/>
-                <div className='Product_detail'>HI</div>
+
+        <div className='products_section'>
+            <div className='head'>
+                <h3 style={{ fontSize: "30px" }}>Deal of the day</h3>
+                <button className="all_btn" style={{ fontSize: "30px" }}>View All</button>
             </div>
-            
-        
+            <Divider sx={{ bgcolor: "black" }} />
+            <Carousel
+                responsive={responsive}
+                infinite={true}
+                draggable={true}
+                swipeable={true}
+                centerMode={true}
+                autoPlay={true}
+                autoPlaySpeed={4000}
+                keyBoardControl={true}
+                showDots={false}
+                navButtonsAlwaysVisible={true}
+                dotListClass="custom-dot-list-style"
+                containerClass="carousel-container"
+                itemClass="carousel-item-padding-40-px"
+                
+            >
+                {
+                    products.map((e) => {
+                        return (
+                            
+                                <div className="products_items">
+                                    <div className="product_img">
+                                        <img src={e.url} alt="product" />
+                                    </div>
+                                    <p className="products_name">{e.title.shortTitle}</p>
+                                    <p className="products_offer" style={{ color: "#  007185" }}>{e.discount}</p>
+                                </div>
+                                
+                            
+                        )
+                    })
+                }
+
+            </Carousel>
+        </div>
+
+
     )
 
 
--- client/src/nav/Home.js
+++ client/src/nav/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './common.css';
 import Banner from './HOME/banner.js';
 import Product from './HOME/Product.js';
+import { useSelector, useDispatch } from "react-redux";
+
 const Home =() => {
+  
   return (
     <div>
     <div  className='Square'>
       <Banner/>
-      
     </div>
       <div className='Product'><Product/></div>
-     
     </div>
     
    
