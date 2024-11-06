d3446dd459265bca5c9048fc4e7b4dd891464bf2
hylauav <hylauav@connect.ust.hk>
2024-11-06 11:48:51 +0800

    proxy

--- server/order-service/index.js
+++ server/order-service/index.js
@@ -1,2 +1,12 @@
 const express = require("express");
+
 const app = express();
+const PORT = 8040;
+
+app.get('/', (req, res) => {
+    res.send("order-service");
+})
+
+app.listen(PORT, () => {
+    console.log(`User Service running on http://localhost:${PORT}`);
+});
