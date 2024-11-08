3e3ccd1dc9540331917bc8168e76fd8c886ce444
hylauav <hylauav@connect.ust.hk>
2024-11-08 23:37:39 +0800

    product detail page

--- client/src/nav/HOME/Buy.js
+++ client/src/nav/HOME/Buy.js
@@ -1,51 +1,49 @@
 import { useParams } from "react-router-dom";
-import { products } from './prodcut_detail';
 import { useEffect, useState } from "react";
-import './buy.css'
+import "./buy.css";
 
 const Productintro = () => {
-    const params = useParams()
-    const [items, setItems] = useState([])
-    const getProductDetail = () => {
-        fetch("http://localhost:8000/product")
-            .then(res => res.json())
-            .then((resJson) => {
-                const data = resJson
-                setItems(data)
-            }
+  const params = useParams();
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getProductDetail = () => {
+    fetch("http://localhost:8000/product")
+      .then((res) => res.json())
+      .then((resJson) => {
+        const data = resJson;
+        console.log(data);
+        setItems(data);
+        setLoading(false);
+      });
+  };
 
-            )
+  useEffect(() => {
+    getProductDetail();
+  }, []);
 
+  //const product = items.find(item => item.product_id === 1);
+  const ID = params.id;
+  const prod = items.find((i) => i.product_id === parseInt(ID));
+  if (loading) return <></>;
 
-    }
-    useEffect(() => {
-
-    }, [])
-    //const product = items.find(item => item.product_id === 1);
-    const ID = params.id
-    const prod = products.find(i => i.id == ID)
-    console.log(ID)
-    return (
-        <div className="detail">
-            <div right>
-
-
-                <div className="test">
-                    <img src={prod.url} style={{ margin: "0 auto" }} />
-                    <button>Add to cart</button>
-                </div>
-            </div>
-            <div className="purchase">
-                <p className="Title">{prod.title.longTitle}</p>
-                <div className="Price">Cost: {prod.price.cost}</div>
-                <div style={{fontSize:"20px"}}> About the item:</div>
-                <p className="description">
-                    
-                    <div> {prod.description}</div>
-                </p>
-            </div>
+  return (
+    <div className="detail">
+      <div right>
+        <div className="test">
+          <img src={prod.image_url} style={{ margin: "0 auto" }} />
+          <button>Add to cart</button>
         </div>
-    )
-}
+      </div>
+      <div className="purchase">
+        <p className="Title">{prod.title.longTitle}</p>
+        <div className="Price">Cost: {prod.price.cost}</div>
+        <div style={{ fontSize: "20px" }}> About the item:</div>
+        <p className="description">
+          <div> {prod.description}</div>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Productintro
+export default Productintro;
--- client/src/nav/HOME/Product.js
+++ client/src/nav/HOME/Product.js
@@ -1,77 +1,76 @@
-import Carousel from 'react-multi-carousel'
-import React from 'react'
-import './Product.css'
-import 'react-multi-carousel/lib/styles.css';
-import { Divider } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import Carousel from "react-multi-carousel";
+import React from "react";
+import "./Product.css";
+import "react-multi-carousel/lib/styles.css";
+import { Divider } from "@mui/material";
+import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const responsive = {
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 4
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2
-    },
-    mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1
-    }
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
 };
 
 const Product = () => {
-    const [items, setItems] = useState([])
-    const [a, setA] = useState([]);
-    
-    const getProductDetail = () => {
-        fetch("http://localhost:8000/product")
-            .then(res => res.json())
-            .then((resJson) => {
-                const data = resJson
-                setItems(data)
-            }
-        )
-    }
-    useEffect(() => {
-        getProductDetail();
-    }, [])
-    return (
-        
-        <div className='products_section'>
-            <div className='head'>
-                <h3 style={{ fontSize: "30px" }}>Deal of the day</h3>
-                <button className="all_btn" style={{ fontSize: "30px" }}>View All</button>
-            </div>
-            <Divider sx={{ bgcolor: "black" }} />
-            <Carousel
-                responsive={responsive}
-                infinite={true}
-                centerMode={true}
-                keyBoardControl={true}
-                showDots={false}
-                navButtonsAlwaysVisible={true}
-                dotListClass="custom-dot-list-style"
-                containerClass="carousel-container"
-                itemClass="carousel-item-padding-40-px"
-            >
-                {
-                    items.map((e) => {
-                        return (
-                            <NavLink to={`/product/${e.product_id}`}>
-                                <div className="products_items">
-                                    <div className="product_img">
-                                        <img src={e.image_url} alt="product" />
-                                    </div>
-                                    <p className="products_name">{e.title}</p>
-                                </div>
-                            </NavLink>
-                        )
-                    })
-                }
-            </Carousel>
-        </div>
-    )
-}
+  const [items, setItems] = useState([]);
+  const [a, setA] = useState([]);
+
+  const getProductDetail = () => {
+    fetch("http://localhost:8000/product")
+      .then((res) => res.json())
+      .then((resJson) => {
+        const data = resJson;
+        setItems(data);
+      });
+  };
+  useEffect(() => {
+    getProductDetail();
+    console.log("abc");
+  }, []);
+  return (
+    <div className="products_section">
+      <div className="head">
+        <h3 style={{ fontSize: "30px" }}>Deal of the day</h3>
+        <button className="all_btn" style={{ fontSize: "30px" }}>
+          View All
+        </button>
+      </div>
+      <Divider sx={{ bgcolor: "black" }} />
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        centerMode={true}
+        keyBoardControl={true}
+        showDots={false}
+        navButtonsAlwaysVisible={true}
+        dotListClass="custom-dot-list-style"
+        containerClass="carousel-container"
+        itemClass="carousel-item-padding-40-px"
+      >
+        {items.map((e) => {
+          return (
+            <NavLink to={`/product/${e.product_id}`} state={{ product: e }}>
+              <div className="products_items">
+                <div className="product_img">
+                  <img src={e.image_url} alt="product" />
+                </div>
+                <p className="products_name">{e.title}</p>
+              </div>
+            </NavLink>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
 export default Product;
