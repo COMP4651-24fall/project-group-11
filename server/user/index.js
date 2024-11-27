2df59ceb4dee6bf8c4d2e4d8c9cfaec30c1f89af
hylauav <hylauav@connect.ust.hk>
2024-11-27 22:11:07 +0800

    remove redundant code

--- server/user/index.js
+++ server/user/index.js
@@ -37,7 +37,7 @@ userRouter.post('/signup', (req, res) => {
             console.error('Error creating user:', err);
             return res.status(500).json({ error: 'Error creating user' });
         }
-        res.status(201).json({ message: 'User created', productId: results.insertId });
+        res.status(201).json({ message: 'User created' });
     });
 })
 
